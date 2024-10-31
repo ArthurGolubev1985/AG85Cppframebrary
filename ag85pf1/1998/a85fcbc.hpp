e13a5c25e12da0108bd64eb9626a4bb98ac51665
Arthur Golubev (AG) <mail@ArthurGolubev.com>
2024-10-31 17:47:29 +0300

    Add files via upload

---
+++ ag85pf1/1998/a85fcbc.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+#ifndef A85FCBC_HPP //AG19850316Cppframebrary1PartCore1Applicationblank1Buildconfiguration
+#define A85FCBC_HPP //AG19850316Cppframebrary1PartCore1Applicationblank1Buildconfiguration
+
+//#define AG19850316_1CPPGENERALLIBRARY_TIMER_DISABLED
+
+#endif //A85FCBC_HPP - AG19850316Cppframebrary1PartCore1Applicationblank1Buildconfiguration
+
+// Author: Arthur Golubev 1985 (ArthurGolubev1985)
