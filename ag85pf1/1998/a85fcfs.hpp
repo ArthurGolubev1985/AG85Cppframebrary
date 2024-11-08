48315dceee3a9eb0fa795da5e4fa63cd81683957
Arthur Golubev (AG) <mail@ArthurGolubev.com>
2024-11-08 15:44:15 +0300

    Add files via upload

---
+++ ag85pf1/1998/a85fcfs.hpp
@@ -0,0 +1,23 @@
+
+#pragma once
+#ifndef A85FCFS_HPP //AG19850316Cppframebrary1PartCore1Framebrarystaticconfiuration
+#define A85FCFS_HPP //AG19850316Cppframebrary1PartCore1Framebrarystaticconfiuration
+
+namespace AG85{
+
+	namespace Cppframebrary1{
+	
+		namespace StaticConfiguration{
+
+			enum { FRAMEBRARY_VERSION_NUMBER = 1 };
+			
+		}; //class StaticConfiguration
+
+	} //namespace Cppframebrary1
+
+} //namespace AG19850316
+
+#endif //A85FCFS_HPP - AG19850316Cppframebrary1PartCore1Applicationblank1Applicationstaticconfiguration
+
+// Author: Arthur Golubev 1985 (ArthurGolubev1985)
+// The file is a part of AG19850316 C++ Framebrary 1 (ag85cppframebrary1)
